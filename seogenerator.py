bc61f4c3501ab7827c6f97d1d7a587abb127dd3a
unknown <yellowsoysauce@gmail.com>
2023-02-15 22:48:41 +0100

    gui update 1

--- seogenerator.py
+++ seogenerator.py
@@ -1,4 +1,30 @@
 # GUI
+from tkinter import *
+from tkinter import filedialog as fd
+
+root = Tk()
+root.geometry("600x250")
+
+pathLabel = Label(root, text="Ścieżka pliku:")
+
+inputBox = Entry(root, width=40)
+inputBox.insert(0, "Wybierz opis")
+
+def openExplorer():
+  # Open explore window
+  filename = fd.askopenfilename()
+  inputBox.delete(0, END)
+  inputBox.insert(0, filename)
+  # Put the file path into input
+
+browseButton = Button(root, text="Przeglądaj pliki", command=openExplorer)
+
+
+pathLabel.grid(row=0, column=0,padx=20, pady=20)
+inputBox.grid(row=0, column=1, padx=10, pady=20)
+browseButton.grid(row=0, column=2, padx=20, pady=20)
+
+root.mainloop()
 
 
 # Break template down into parts
