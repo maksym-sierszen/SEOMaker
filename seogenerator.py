a62e32dd6d2806f5449541c055b72f5f05fe4f4b
unknown <yellowsoysauce@gmail.com>
2023-03-03 21:55:46 +0100

    added content filling and saving it to the file

--- seogenerator.py
+++ seogenerator.py
@@ -3,33 +3,12 @@ from tkinter import filedialog as fd
 import docx2txt
 import os
 
-def textRemover(toRemove, fromRemove):
-  if toRemove in fromRemove:
-      fromRemove = fromRemove.replace(toRemove, "")
-  return fromRemove
 
 
-
-# Open the file
-def openWord(wordFile):
-  document = docx2txt.process(wordFile)
-
-  paragraphs = []
-  paragraphs = document.split('\n\n')
-  # Warunek że jeśli paragraph[0] == filename to skip
-
-  wordFile = textRemover(".docx", wordFile)
-  if (paragraphs[0] == wordFile):
-    paragraphs.pop(0)
-
-
-  print(paragraphs[0])
-  print("\n\n")
-  print(paragraphs[1])
-  print("\n\n")
-  print(paragraphs[2])
-
-
+def textReplacer(toReplace, fromReplace, withReplace):
+  if toReplace in fromReplace:
+      fromReplace = fromReplace.replace(toReplace, withReplace)
+  return fromReplace
 
 # Choose file and create filename for graphics
 def openExplorer():
@@ -40,11 +19,78 @@ def openExplorer():
 
   # Put the file path into input
   imageNameBox.delete(0, END)
+  global fileName 
   fileName = os.path.basename(filePath)
   fileNameReady = lowercaseifyName(fileName)
   imageNameBox.insert(0, fileNameReady)
 
-  openWord(fileName)
+  getFromWord(fileName)
+  makeText()
+
+# Work on docx file
+def getFromWord(wordFile):
+  document = docx2txt.process(wordFile)
+
+  global paragraphs
+  paragraphs = []
+  paragraphs = document.split('\n\n')
+
+  # If first paragraph is the same as products name then skip it (product name is taken from the file name)
+  wordFile = textReplacer(".docx", wordFile, "")
+  if (paragraphs[0] == wordFile):
+    paragraphs.pop(0)
+
+  # print(paragraphs[0])
+  # print("\n---przerwa ---\n")
+  # print(paragraphs[1])
+  # print("\n---przerwa ---\n")
+  # print(paragraphs[2])
+  # print("\n---przerwa ---\n")
+  # print(paragraphs[3])
+  # print("\n---przerwa ---\n")
+  # print(paragraphs[4])
+  #################
+  # taki pomysl że parzyste to tresc a nieparzyste to naglowki
+
+# Working with the txt
+def makeText():
+  # Open the template
+  with open("template.txt", "r", encoding="utf-8") as file:
+      # odczytanie całej zawartości pliku
+      template = file.read()
+      
+
+  templateParts = []
+  templateParts = template.split('\n\n')
+
+  # print(templateParts[0])
+  # print("\n---przerwa ---\n")
+  # print(templateParts[1])
+  # print("\n---przerwa ---\n")
+  # print(templateParts[2])
+  # print("\n---przerwa ---\n")
+  # print(templateParts[3])
+  # print("\n---przerwa ---\n")
+  # print(templateParts[4])
+  #################
+
+  # Fill the template
+
+  readyToUse = textReplacer(templateParts[1], template, fileName)
+  readyToUse = textReplacer(templateParts[3], readyToUse, paragraphs[0])
+
+  readyToUse = textReplacer(templateParts[6], readyToUse, paragraphs[1])
+  readyToUse = textReplacer(templateParts[8], readyToUse, paragraphs[2])
+
+  readyToUse = textReplacer(templateParts[11], readyToUse, paragraphs[3])
+  readyToUse = textReplacer(templateParts[13], readyToUse, paragraphs[4])
+
+  readyToUse = textReplacer(templateParts[16], readyToUse, paragraphs[5])
+  readyToUse = textReplacer(templateParts[18], readyToUse, paragraphs[6])
+  # Save to new file
+  with open("wpisz_kod_ERP.txt", "w", encoding="utf-8") as file:
+      file.write(readyToUse)
+
 
 
 # Convert filename into a product-name-image- format
@@ -56,7 +102,7 @@ def lowercaseifyName(name):
       name = name.replace(symbol, "-")
 
     # Remove file extension from name
-    name = textRemover("docx", name)
+    name = textReplacer("docx", name, "")
     # if "docx" in name:
     #   name = name.replace("docx", "")
   return name
